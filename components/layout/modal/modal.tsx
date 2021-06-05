a00a1084bd52b37d93307300c755e5942008763d
SpyOfAlien <doanquangbao.it@gmail.com>
2021-06-05 20:39:42 +0700

    [develop] close sub modal

--- components/layout/modal/modal.tsx
+++ components/layout/modal/modal.tsx
@@ -1,9 +1,10 @@
-import { FC } from 'react';
+import { FC, useEffect, useRef } from 'react';
 import cn from 'classnames';
 import s from './modal.module.scss';
 import Portal from '@reach/portal';
 import { motion, AnimatePresence } from 'framer-motion';
 import SubModal from './sub-modal';
+import { useUI } from '../../../lib/context/ui-context';
 
 interface Props {
   open: boolean;
@@ -14,6 +15,28 @@ interface Props {
 }
 
 const Modal: FC<Props> = ({ children, open, onClose, isFull, subModal }) => {
+  const { closeSubModal, displaySubModal } = useUI();
+  const subModalRef = useRef<HTMLDivElement>();
+
+  useEffect(() => {
+    /**
+     * Alert if clicked on outside of element
+     */
+    function handleClickOutside(event) {
+      const node = subModalRef.current;
+      if (node && !node.contains(event.target) && displaySubModal) {
+        closeSubModal();
+      }
+    }
+
+    // Bind the event listener
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      // Unbind the event listener on clean up
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [subModalRef]);
+
   return (
     <Portal>
       <AnimatePresence>
@@ -31,7 +54,7 @@ const Modal: FC<Props> = ({ children, open, onClose, isFull, subModal }) => {
             <motion.div className={cn(s.modal, s.subModal)}>
               <div className="sw-relative sw-w-full sw-h-full sw-flex sw-justify-center sw-items-center">
                 <div className="sw-absolute sw-inset-0 sw-bg-background sw-opacity-85"></div>
-                <div className={cn(s.subContent)}>
+                <div className={cn(s.subContent)} ref={subModalRef}>
                   <SubModal>{children}</SubModal>
                 </div>
               </div>
