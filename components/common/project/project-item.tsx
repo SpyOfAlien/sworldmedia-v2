234fb5be7286ad3b0baa31ff30a4d77d7446e893
SpyOfAlien <doanquangbao.it@gmail.com>
2022-05-03 14:46:27 +0700

    update home project

--- components/common/project/project-item.tsx
+++ components/common/project/project-item.tsx
@@ -15,9 +15,9 @@ const ProjectItem = ({ project }) => {
   return (
     <div
       style={{ minHeight: '450px' }}
-      className="sw-glass sw-text-white sw-flex sw-flex-col lg:sw-flex-row sw-p-4 lg:sw-p-12 sw-rounded-md"
+      className="sw-glass sw-text-white sw-flex sw-flex-col-reverse sw-p-4 lg:sw-p-12 sw-rounded-md"
     >
-      <div className="sw-w-full lg:sw-w-3/5 sw-pr-12">
+      <div className="sw-w-full sw-pr-12">
         <h6 className="sw-text-barlow sw-text-h6 sw-mb-4 sw-font-semibold ">
           {router.locale === 'en' ? project.enName : project.vnName}
         </h6>
@@ -26,7 +26,7 @@ const ProjectItem = ({ project }) => {
         </p>
         <button onClick={onReadmore}>{t('action__findoutmore')}</button>
       </div>
-      <div className="sw-w-full lg:sw-w-2/5">
+      <div className="sw-w-full">
         <div className="sw-w-full sw-mb-4">
           <Image
             src={project.thumbnail.url}
